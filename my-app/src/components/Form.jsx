b86da27dd80ec6aad5cdf6cf3065ea1e68858a4d
Bao-gio-co-tien <tranhong0347@gmail.com>
2025-09-17 17:41:56 +0700

    fix

--- my-app/src/components/Form.jsx
+++ my-app/src/components/Form.jsx
@@ -1,24 +1,36 @@
-export function Form() {
+import { useState } from "react";
+
+function Form() {
     const [name, setName] = useState('');
-    const [email,setEmail] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     return(
         <form>
             <div>
                 <label>Name</label>
-                <input type="text" name="name" onChange={(e)=>setName(e.target.value)}/>
+                <input type="text" name="name" 
+                onChange={(e)=>setName(e.target.value)}
+                />
             </div>
 
             <div>
                 <label htmlFor="">Email</label>
-                <input type="email" name="name" onChange={(e) =>setEmail(e.target.value)} />
+                <input type="email" name="email" 
+                onChange={(e) =>setEmail(e.target.value)} 
+                />
             </div>
 
             <div>
                 <label htmlFor="">Password</label>
-                <input type="password" name="password" onChange={(e) =>setPassword(e.target.value)} />
+                <input type="password" name="password" 
+                onChange={(e) =>setPassword(e.target.value)} 
+                />
             </div>
+
+            <submit>Submit</submit>
         </form>
     )
 }
+
+export default Form
